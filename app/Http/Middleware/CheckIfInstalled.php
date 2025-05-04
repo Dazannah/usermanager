67ef1afec8c0d930eefa9ed175cf8759aa0db64c
Dazannah <beattius@gmail.com>
2025-05-04 16:44:38 +0200

    added middle ware to redirect setup if not done yet

---
+++ app/Http/Middleware/CheckIfInstalled.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckIfInstalled {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response {
+        if (!config("app.installed") && ($request->path() != "initial-setup" && $request->path() != 'livewire/update'))
+            return redirect("initial-setup");
+
+        if (config("app.installed") && $request->path() == "initial-setup")
+            return redirect("");
+
+        return $next($request);
+    }
+}
