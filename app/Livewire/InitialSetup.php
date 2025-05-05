1bcd20f7647718bbb7fee346206a4ad903958912
Dazannah <beattius@gmail.com>
2025-05-05 21:04:38 +0200

    init setup updated to create default admin account, run migration

--- app/Livewire/InitialSetup.php
+++ app/Livewire/InitialSetup.php
@@ -2,18 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
+use Exception;
 use Livewire\Component;
-use Illuminate\Support\Facades\Config;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Artisan;
 
 class InitialSetup extends Component {
     public $app_name = 'Felhasználó kezelő';
 
-    public $db_host = 'asd';
+    public $db_host = '';
     public $db_port = 3306;
-    public $db_databasename = 'asd';
-    public $db_username = 'asd';
-    public $db_password = 'asd';
+    public $db_databasename = '';
+    public $db_username = '';
+    public $db_password = '';
+
+    public $admin_username = '';
+    public $admin_email = '';
+    public $password = '';
+    public $password_confirmation = '';
 
     protected $rules = [
         'app_name' => 'required',
@@ -21,7 +30,10 @@ class InitialSetup extends Component {
         'db_port' => 'required|integer|min:1|max:65535',
         'db_databasename' => 'required',
         'db_username' => 'required',
-        'db_password' => 'required'
+        'db_password' => 'required',
+        'admin_username' => 'required|max:255',
+        'admin_email' => 'required|email|max:255',
+        'password' => "required|confirmed"
     ];
 
     protected $messages = [
@@ -30,7 +42,13 @@ class InitialSetup extends Component {
         'db_port.required' => 'Adatbázis szerver port megadása kötelező.',
         'db_databasename.required' => 'Adatbázis név megadása kötelező.',
         'db_username.required' => 'Adatbázis felhasználónév megadása kötelező.',
-        'db_password.required' => 'Adatbázis felhasználó jelszó cím megadása kötelező.'
+        'db_password.required' => 'Adatbázis felhasználó jelszó cím megadása kötelező.',
+        'admin_username.required' => 'Admin felhasználónév megadása kötelező.',
+        'admin_username.max' => 'Admin felhasználónév túl hosszú.',
+        'admin_email.required' => 'Admin email cím megadása kötelező.',
+        'admin_email.email' => 'Email cím megadása kötelező.',
+        'password.required' => 'Admin jelszó megadása kötelező.',
+        'password.confirmed' => 'Megadott jelszavak nem egyeznek.',
     ];
 
     public function updated($propertyName) {
@@ -38,19 +56,65 @@ class InitialSetup extends Component {
     }
 
     public function save() {
+
         $this->validate();
+        $this->validate(['password' => [Rules\Password::defaults()]]);
 
-        file_put_contents(
-            base_path('.env'),
-            preg_replace('/\nAPP_NAME=.*/', "\nAPP_NAME=" . "'$this->app_name'", file_get_contents(base_path('.env')))
-        );
+        //sikeres validáció után .env módosítása és mentése
+        $env_content = $original_env_content = file_get_contents(base_path('.env'));
 
-        Artisan::call('config:cache');
+        try {
+            $env_content = preg_replace('/\nAPP_NAME=.*/', "\nAPP_NAME=" . "'$this->app_name'", $env_content);
 
-        redirect('/');
+            $env_content = preg_replace('/\nDB_HOST=.*/', "\nDB_HOST=" . "'$this->db_host'", $env_content);
+            $env_content = preg_replace('/\nDB_PORT=.*/', "\nDB_PORT=" . "'$this->db_port'", $env_content);
+            $env_content = preg_replace('/\nDB_DATABASE=.*/', "\nDB_DATABASE=" . "'$this->db_databasename'", $env_content);
+            $env_content = preg_replace('/\nDB_USERNAME=.*/', "\nDB_USERNAME=" . "'$this->db_username'", $env_content);
+            $env_content = preg_replace('/\nDB_PASSWORD=.*/', "\nDB_PASSWORD=" . "'$this->db_password'", $env_content);
+
+            $env_content = preg_replace('/APP_INSTALLED=.*/', "APP_INSTALLED=true", $env_content);
+            $env_content = preg_replace('/SESSION_DRIVER=.*/', "SESSION_DRIVER=database", $env_content);
+
+            file_put_contents(
+                base_path('.env'),
+                $env_content
+            );
+
+            //módosított .env betöltése memóriába
+            Artisan::call('config:clear');
+            Artisan::call('config:cache');
+
+            //adatbázis migráció
+            Artisan::call('migrate', array(
+                '--path' => 'database/migrations',
+                '--database' => 'mysql',
+                '--force' => true
+            ));
+
+            //alapértelmezett rendszergazda fiók létrehozása
+            $user_data['name'] = $this->admin_username;
+            $user_data['email'] = $this->admin_email;
+            $user_data['password'] = Hash::make($this->password);
+
+            User::create($user_data);
+
+            return redirect()->to('/');
+        } catch (Exception $err) {
+            session()->flash('message', $err->getMessage());
+
+            //hiba esetén a változatlan .env vissza töltése
+            file_put_contents(
+                base_path('.env'),
+                $original_env_content
+            );
+
+            Artisan::call('config:clear');
+        }
     }
 
     public function render() {
-        return view('livewire.initial-setup')->layout('layouts.guest');
+        return view('livewire.initial-setup', [
+            'errors' => session()->get('errors') ?: new \Illuminate\Support\ViewErrorBag()
+        ])->layout('layouts.guest');
     }
 }
--- resources/views/livewire/initial-setup.blade.php
+++ resources/views/livewire/initial-setup.blade.php
@@ -7,13 +7,20 @@
       <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
           {{ __('Ezek a beállítások később módosíthatóak az .env file-ban.') }}
       </p>
+      <div>
+        @if (session()->has('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+        @endif
+    </div>
   </header>
     <form wire:submit.prevent="save" class="mt-6 space-y-6">
       <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
         <div class="max-w-xl">
             <x-input-label for="app_name" :value="__('Alkalmazás neve')" />
             <x-text-input wire:model.live="app_name" id="app_name" name="app_name" type="text" class="mt-1 block w-full" />
-            <x-input-error :messages="$errors->get('app_name')" class="mt-2" />
+            @error('app_name') <x-input-error :messages="$message" class="mt-2" /> @enderror
         </div>
       </div>
       <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
@@ -26,32 +33,53 @@
         <div class="max-w-xl">
             <x-input-label for="db_host" :value="__('Szerver címe')" />
             <x-text-input wire:model="db_host" id="db_host" name="db_host" type="text" class="mt-1 block w-full" />
-            <x-input-error :messages="$errors->get('db_host')" class="mt-2" />
+            @error('db_host') <x-input-error :messages="$message" class="mt-2" /> @enderror
   
             <x-input-label for="db_port" :value="__('Port')" />
             <x-text-input wire:model="db_port" id="db_port" name="db_port" type="number" class="mt-1 block w-full" />
-            <x-input-error :messages="$errors->get('db_port')" class="mt-2" />
+            @error('db_port') <x-input-error :messages="$message" class="mt-2" /> @enderror
   
             <x-input-label for="db_databasename" :value="__('Adatbázis név')" />
             <x-text-input wire:model="db_databasename" id="db_databasename" name="db_databasename" type="text" class="mt-1 block w-full" />
-            <x-input-error :messages="$errors->get('db_databasename')" class="mt-2" />
+            @error('db_databasename') <x-input-error :messages="$message" class="mt-2" /> @enderror
   
             <x-input-label for="db_username" :value="__('Felhasználó név')" />
             <x-text-input wire:model="db_username" id="db_username" name="db_username" type="text" class="mt-1 block w-full" />
-            <x-input-error :messages="$errors->get('db_username')" class="mt-2" />
+            @error('db_username') <x-input-error :messages="$message" class="mt-2" /> @enderror
   
             <x-input-label for="db_password" :value="__('Jelszó')" />
             <x-text-input wire:model="db_password" id="db_password" name="db_password" type="password" class="mt-1 block w-full" />
-            <x-input-error :messages="$errors->get('db_password')" class="mt-2" />
+            @error('db_password') <x-input-error :messages="$message" class="mt-2" /> @enderror
         </div>
       </div>
+              <!--
+    public $password = '';
+    public $password_confirmation = '';
+        -->
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+          {{ __('Alapértelmezett rendszergazda fiók') }}
+      </h2>
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+          <div class="max-w-xl">
+              <x-input-label for="admin_username" :value="__('Felhasználónév')" />
+              <x-text-input wire:model="admin_username" id="admin_username" name="admin_username" type="text" class="mt-1 block w-full" />
+              @error('admin_username') <x-input-error :messages="$message" class="mt-2" /> @enderror
+
+              <x-input-label for="admin_email" :value="__('Email cím')" />
+              <x-text-input wire:model="admin_email" id="admin_email" name="admin_email" type="text" class="mt-1 block w-full" />
+              @error('admin_email') <x-input-error :messages="$message" class="mt-2" /> @enderror
+
+              <x-input-label for="password" :value="__('Jelszó')" />
+              <x-text-input wire:model="password" id="password" name="password" type="password" class="mt-1 block w-full" />
+              @error('password') <x-input-error :messages="$message" class="mt-2" /> @enderror
+
+              <x-input-label for="password_confirmation" :value="__('Jelszó megerősítése')" />
+              <x-text-input wire:model="password_confirmation" id="password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" />
+              @error('password_confirmation') <x-input-error :messages="$message" class="mt-2" /> @enderror
+          </div>
+        </div>
       <div class="flex items-center gap-4">
           <x-primary-button>{{ __('Mentés') }}</x-primary-button>
-  
-  
-          <x-action-message class="me-3" on="saved">
-              {{ __('Mentés sikeres.') }}
-          </x-action-message>
       </div>
   </form>
   </div>
