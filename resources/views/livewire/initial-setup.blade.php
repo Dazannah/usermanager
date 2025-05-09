f3f811f9274552ada273e8f5a65035ddbb1d8262
Dazannah <beattius@gmail.com>
2025-05-09 20:59:21 +0200

    mailer config view

--- resources/views/livewire/initial-setup.blade.php
+++ resources/views/livewire/initial-setup.blade.php
@@ -21,8 +21,58 @@
             <x-input-label for="app_name" :value="__('Alkalmazás neve')" />
             <x-text-input placeholder="Alkalmazás neve" wire:model.live="app_name" id="app_name" name="app_name" type="text" class="mt-1 block w-full" />
             @error('app_name') <x-input-error :messages="$message" class="mt-2" /> @enderror
+{{-- 
+            <x-input-label for="logo" :value="__('Logo')" />
+            @if ($logo) 
+              <img src="{{ $logo->temporaryUrl() }}">
+            @endif
+            <x-text-input wire:model="logo" id="logo" name="logo" type="file" class="mt-1 block w-full" />
+            @error('logo') <x-input-error :messages="$message" class="mt-2" /> @enderror
+            <x-action-message wire:loading class="me-3" on="password-updated">
+              {{ __('Loading') }}
+            </x-action-message> --}}
         </div>
       </div>
+
+    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+        {{ __('Technikai email fiók beállítása') }}
+    </h2>
+    <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+      {{ __('SMTP') }}
+  </p>
+      <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+        <div class="max-w-xl">
+            <x-input-label for="mail_host" :value="__('Email szerver címe')" />
+            <x-text-input placeholder="Szerver címe" wire:model="mail_host" id="mail_host" name="mail_host" type="text" class="mt-1 block w-full" />
+            @error('mail_host') <x-input-error :messages="$message" class="mt-2" /> @enderror
+  
+            <x-input-label for="mail_port" :value="__('Port')" />
+            <x-text-input placeholder="465" wire:model="mail_port" id="mail_port" name="mail_port" type="number" class="mt-1 block w-full" />
+            @error('mail_port') <x-input-error :messages="$message" class="mt-2" /> @enderror
+  
+            <x-input-label for="mail_username" :value="__('Email cím')" />
+            <x-text-input placeholder="Email cím" wire:model="mail_username" id="mail_username" name="mail_username" type="email" class="mt-1 block w-full" />
+            @error('mail_username') <x-input-error :messages="$message" class="mt-2" /> @enderror
+  
+            <x-input-label for="mail_password" :value="__('Jelszó')" />
+            <x-text-input placeholder="Jelszó" wire:model="mail_password" id="mail_password" name="mail_password" type="password" class="mt-1 block w-full" />
+            @error('mail_password') <x-input-error :messages="$message" class="mt-2" /> @enderror
+
+            <x-input-label for="mail_test_address" :value="__('Teszt címzet')" />
+            <x-text-input placeholder="Teszt címzet" wire:model="mail_test_address" id="mail_test_address" name="mail_test_address" type="text" class="mt-1 block w-full" />
+            @error('mail_test_result_error') <x-input-error :messages="$message" class="mt-2" /> @enderror
+            @if (session()->has('mail_test_result'))
+                <div class="alert alert-success">
+                    {{ session('mail_test_result') }}
+                </div>
+            @endif
+            <x-primary-button wire:click.prevent="test_mail_connection_standalone">{{ __('Teszt email küldés') }}</x-primary-button>
+            <x-action-message wire:loading class="me-3" on="test_mail_connection">
+              {{ __('Loading') }}
+            </x-action-message>
+        </div>
+      </div>
+
       <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
         {{ __('Adatbázis beállítások (mysql)') }}
     </h2>
@@ -67,6 +117,10 @@
       </h2>
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
           <div class="max-w-xl">
+            <x-input-label for="admin_name" :value="__('Név')" />
+            <x-text-input placeholder="Név" wire:model="admin_name" id="admin_name" name="admin_name" type="text" class="mt-1 block w-full" />
+            @error('admin_name') <x-input-error :messages="$message" class="mt-2" /> @enderror
+
               <x-input-label for="admin_username" :value="__('Felhasználónév')" />
               <x-text-input placeholder="Felhasználónév" wire:model="admin_username" id="admin_username" name="admin_username" type="text" class="mt-1 block w-full" />
               @error('admin_username') <x-input-error :messages="$message" class="mt-2" /> @enderror
