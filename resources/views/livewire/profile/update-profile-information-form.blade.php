67fa26f828a7c31daca5ed4c57d840a6caf44785
Dazannah <beattius@gmail.com>
2025-05-09 21:00:25 +0200

    email changed to nullabble

--- resources/views/livewire/profile/update-profile-information-form.blade.php
+++ resources/views/livewire/profile/update-profile-information-form.blade.php
@@ -9,7 +9,7 @@ use Livewire\Volt\Component;
 new class extends Component
 {
     public string $name = '';
-    public string $email = '';
+    public string|null $email = '';
 
     /**
      * Mount the component.
