59b0eed50bcc99badc98f2e44579ec56e7fa54e0
Dazannah <beattius@gmail.com>
2025-05-09 20:56:50 +0200

    mail test view added

---
+++ app/Mail/MailTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class MailTest extends Mailable {
+    use Queueable, SerializesModels;
+
+    private $app_name = '';
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(string $app_name) {
+        $this->app_name = $app_name;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope {
+        return new Envelope(
+            subject: 'Mail Test',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content {
+        return new Content(
+            view: 'Mail.Test',
+            with: ['app_name' => $this->app_name]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array {
+        return [];
+    }
+}
