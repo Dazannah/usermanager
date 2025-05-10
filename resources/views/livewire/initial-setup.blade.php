e5897055719f919a246fc1e5403097c5c7888249
Dazannah <beattius@gmail.com>
2025-05-10 14:36:54 +0200

    hidden divs changed to use wire:show

--- resources/views/livewire/initial-setup.blade.php
+++ resources/views/livewire/initial-setup.blade.php
@@ -35,18 +35,15 @@
       </div>
 
     <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-        {{ __('Technikai email fiók beállítása') }}
+        {{ __('Technikai email fiók beállítása SMTP') }}
     </h2>
-    <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-      {{ __('SMTP') }}
-  </p>
       <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
         <div class="max-w-xl">
             <x-input-label for="mail_host" :value="__('Email szerver címe')" />
             <x-text-input placeholder="Szerver címe" wire:model="mail_host" id="mail_host" name="mail_host" type="text" class="mt-1 block w-full" />
             @error('mail_host') <x-input-error :messages="$message" class="mt-2" /> @enderror
   
-            <x-input-label for="mail_port" :value="__('Port')" />
+            <x-input-label for="mail_port" :value="__('SMTP Port')" />
             <x-text-input placeholder="465" wire:model="mail_port" id="mail_port" name="mail_port" type="number" class="mt-1 block w-full" />
             @error('mail_port') <x-input-error :messages="$message" class="mt-2" /> @enderror
   
@@ -67,14 +64,14 @@
                 </div>
             @endif
             <x-primary-button wire:click.prevent="test_mail_connection_standalone">{{ __('Teszt email küldés') }}</x-primary-button>
-            <x-action-message wire:loading class="me-3" on="test_mail_connection">
+            <x-action-message wire:loading class="me-3" on="test_mail_connection_standalone">
               {{ __('Loading') }}
             </x-action-message>
         </div>
       </div>
 
       <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-        {{ __('Adatbázis beállítások (mysql)') }}
+        {{ __('Adatbázis beállítások MYSQL') }}
     </h2>
     <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
       {{ __('Adatbázist előre létre kell hozni.') }}
@@ -107,7 +104,7 @@
                 </div>
             @endif
             <x-primary-button wire:click.prevent="test_database_connection_standalone">{{ __('Kapcsolat tesztelése') }}</x-primary-button>
-            <x-action-message wire:loading class="me-3" on="password-updated">
+            <x-action-message wire:loading class="me-3" on="test_database_connection_standalone">
               {{ __('Loading') }}
             </x-action-message>
         </div>
@@ -141,10 +138,10 @@
 
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             {{'LDAP autentikáció'}}
-            <x-text-input wire:click="toggle_ldap_active" type="checkbox" class="m-1 p-1" />
+            <x-text-input wire:model="ldap_active" type="checkbox" class="m-1 p-1" :checked="$ldap_active"/>
         </h2>
-      @if ($ldap_active === true)
-        <div wire:key="ldap-fields" class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+
+        <div wire:show="ldap_active" wire:key="ldap-fields" class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
           <div class="max-w-xl">
             <x-input-label for="ldap_host" :value="__('Szerver címe')" />
             <x-text-input placeholder="Szerver címe" wire:model="ldap_host" id="ldap_host" name="ldap_host" type="text" class="mt-1 block w-full" />
@@ -173,22 +170,20 @@
                   </div>
               @endif
               <x-primary-button wire:click.prevent="test_ldap_connection_standalone">{{ __('LDAP tesztelése') }}</x-primary-button>
-              <x-action-message wire:loading class="me-3" on="password-updated">
+              <x-action-message wire:loading class="me-3" on="test_ldap_connection_standalone">
                 {{ __('Loading') }}
               </x-action-message>
           </div>
         </div>
-        @endif
 
       <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
           {{ __('ISPConfig SOAP api') }}
-          <x-text-input wire:click="toggle_ispfonfig_active" type="checkbox" class="m-1 p-1" />
+          <x-text-input wire:model="ispfonfig_active" type="checkbox" class="m-1 p-1" :checked="$ispfonfig_active"/>
       </h2>
       <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
           {{ __('Az ISPConfig SOAP api, a terjesztési listák létrehozása, szerkesztéséhez kell, amennyiben használja a szervezet az ISPConfig-ot') }}
       </p>
-        @if ($ispfonfig_active === true)
-        <div wire:key="ispconfig-fields" class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+        <div wire:show="ispfonfig_active" wire:key="ispconfig-fields" class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
           <div class="max-w-xl">
             <x-input-label for="ispconfig_soap_uri" :value="__('ISPConfig szerver címe')" />
             <x-text-input placeholder="localhost" wire:model="ispconfig_soap_uri" id="ispconfig_soap_uri" name="ispconfig_soap_uri" type="text" class="mt-1 block w-full" />
@@ -218,12 +213,11 @@
               </x-action-message>
           </div>
         </div>
-        @endif
         @error('save_error') <x-input-error :messages="$message" class="mt-2" /> @enderror
       <div class="flex items-center gap-4">
           <x-primary-button>{{ __('Mentés') }}</x-primary-button>
 
-          <x-action-message wire:loading class="me-3" on="password-updated">
+          <x-action-message wire:loading class="me-3" on="save">
             {{ __('Loading') }}
           </x-action-message>
       </div>
