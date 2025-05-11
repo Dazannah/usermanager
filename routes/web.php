bf88167d02858fdb5759407f442256607d30570a
Dazannah <beattius@gmail.com>
2025-05-11 18:40:49 +0200

    root redirect added and and admin prefix group added

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use App\Livewire\InitialSetup;
 use App\Http\Middleware\IsAdmin;
+use App\Livewire\Admin\AdminDashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('initial-setup', InitialSetup::class);
-Route::view('/', 'welcome');
+Route::get('/', fn() => redirect('/login'));
 
 Route::middleware(['auth'])->group(function () {
     Route::view('dashboard', 'dashboard')
@@ -13,14 +14,12 @@ Route::middleware(['auth'])->group(function () {
     Route::view('profile', 'profile')
         ->name('profile');
 
-    Route::middleware([IsAdmin::class])->group(function () {
+    Route::middleware([IsAdmin::class])->prefix('admin')->group(function () {
+        Route::get('/', AdminDashboard::class)
+            ->name('admin-dashboard');
         Route::get('app-configuration', InitialSetup::class)
             ->name('app-configuration');
     });
 });
-
-
-
-
 
 require __DIR__ . '/auth.php';
