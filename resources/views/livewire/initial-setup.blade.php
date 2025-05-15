bbfee366ca8a265a3c79cd92593a41953b37be2f
Dazannah <beattius@gmail.com>
2025-05-15 21:19:50 +0200

    div size added

--- resources/views/livewire/initial-setup.blade.php
+++ resources/views/livewire/initial-setup.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-6">
   <header>
     <x-slot name="header">
       <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
