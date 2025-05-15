570297207e04818ab1f642927864f4ff57889e90
Dazannah <beattius@gmail.com>
2025-05-15 21:17:52 +0200

    created

---
+++ app/Livewire/Admin/AdminNavigation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminNavigation extends Component {
+    public function render() {
+        return view('livewire.layout.admin-navigation');
+    }
+}
