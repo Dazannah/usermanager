0a3a02e650f7162cfd89227aa6ca4ce52c1ce086
Dazannah <beattius@gmail.com>
2025-05-16 20:01:37 +0200

    statuses seed added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,24 @@
 
 namespace Database\Seeders;
 
+use App\Models\Status;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+    public function run(): void {
+        Status::create([
+            'name' => 'active',
+            'displayName' => 'Aktív'
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Status::create([
+            'name' => 'inactive',
+            'displayName' => 'Inaktív'
         ]);
     }
 }
