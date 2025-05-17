292fcbe99c9b1750c07ea6fc02115ecf3d924c38
Dazannah <beattius@gmail.com>
2025-05-17 16:44:39 +0200

    alpine js and livewire manually injected to be abble to use soer plugin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,15 @@
-import axios from 'axios';
+import {
+    Livewire,
+    Alpine,
+} from "../../vendor/livewire/livewire/dist/livewire.esm";
+import sort from "@alpinejs/sort";
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+Alpine.plugin(sort);
+
+window.Alpine = Alpine;
+
+Livewire.start();
