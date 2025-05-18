7f44083c4761f6e69090ae2881bb9cb54e06d6c6
Dazannah <beattius@gmail.com>
2025-05-18 09:44:25 +0200

    logout method replaced in to component

--- app/Livewire/Admin/AdminNavigation.php
+++ app/Livewire/Admin/AdminNavigation.php
@@ -3,9 +3,16 @@
 namespace App\Livewire\Admin;
 
 use Livewire\Component;
+use App\Livewire\Actions\Logout;
 
 class AdminNavigation extends Component {
     public function render() {
         return view('livewire.layout.admin-navigation');
+    }
+
+    public function logout(Logout $logout): void {
+        $logout();
+
+        $this->redirect('/login', navigate: true);
     }
 }
