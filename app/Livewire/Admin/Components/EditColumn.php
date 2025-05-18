2702e01d15f7ea722883ce2bdc97251e4425fa79
Dazannah <beattius@gmail.com>
2025-05-18 16:23:40 +0200

    edit column funcionality added

---
+++ app/Livewire/Admin/Components/EditColumn.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Livewire\Admin\Components;
+
+use Exception;
+use App\Models\Column;
+use Livewire\Component;
+use Illuminate\Validation\ValidationException;
+
+class EditColumn extends Component {
+    //base properties
+    public $statuses, $columns_number;
+
+    //properties for edit
+    public $edit_column, $edit_column_display_name, $edit_column_status_id, $edit_column_position;
+
+    protected $listeners = ['edit_column_id'];
+
+    public function edit_column_id($edit_column_id) {
+        $this->edit_column = Column::where('id',  $edit_column_id)->first();
+
+        $this->columns_number = count(Column::all());
+
+        $this->edit_column_display_name = $this->edit_column->displayName;
+        $this->edit_column_status_id =  $this->edit_column->status->id;
+        $this->edit_column_position = $this->edit_column->position;
+    }
+
+    public function delete_edit_column() {
+        try {
+            $delete_result = $this->edit_column->delete();
+
+            if (!isset($delete_result))
+                throw new Exception('Törölni kívánt oszlop nem található.');
+
+            $this->reset('edit_column_display_name', 'edit_column_status_id', 'edit_column_position');
+            $this->dispatch('refresh_authorization_mount');
+            $this->dispatch('edit_columns_delete_success');
+        } catch (Exception $err) {
+            $this->addError('save_edit_column_error', $err->getMessage());
+
+            $this->dispatch('refresh_authorization_mount');
+        }
+    }
+
+    public function save_edit_column() {
+        try {
+            $rules = [
+                'edit_column_display_name' => 'required',
+                'edit_column_status_id' => 'required',
+                'edit_column_position' => 'required'
+            ];
+
+            $messages = [
+                'edit_column_display_name.required' => 'Elnevezés kitöltése kötelező',
+                'edit_column_status_id.required' => 'Egy státuszt ki kell választani',
+                'edit_column_position.required' => 'Pozíció megadása kötelező'
+            ];
+
+            $this->validate($rules, $messages);
+
+            $this->change_columns_position();
+
+            $this->edit_column->displayName = $this->edit_column_display_name;
+            $this->edit_column->status_id = $this->edit_column_status_id;
+            $this->edit_column->position = $this->edit_column_position;
+
+            $this->edit_column->save();
+
+            // $this->reset('edit_column_display_name', 'edit_column_status_id', 'edit_column_position');
+            $this->dispatch('refresh_authorization_mount');
+            $this->dispatch('edit_columns_save_success');
+        } catch (ValidationException $err) {
+            throw $err;
+        } catch (Exception $err) {
+            $this->addError('save_edit_column_error', $err->getMessage());
+
+            $this->dispatch('refresh_authorization_mount');
+        }
+    }
+
+    public function change_columns_position() {
+        try {
+            $columns = Column::all();
+
+            $original_position = $this->edit_column->position;
+            $new_position = $this->edit_column_position;
+
+            foreach ($columns as $column) {
+                if ($column->id == $this->edit_column->id)
+                    continue;
+
+                if ($new_position > $original_position && $column->position <= $new_position && $original_position <= $column->position) {
+                    $column->position -= 1;
+                    $column->save();
+                }
+
+                if ($new_position < $original_position && $column->position >= $new_position && $original_position >= $column->position) {
+
+                    $column->position += 1;
+                    $column->save();
+                }
+            };
+        } catch (Exception $err) {
+            $this->addError('save_edit_column_error', $err->getMessage());
+        }
+    }
+
+    public function render() {
+        return view('livewire.admin.components.edit-column');
+    }
+}
---
+++ resources/views/livewire/admin/components/edit-column.blade.php
@@ -0,0 +1,121 @@
+<div x-data="{ show: false }" x-init="$watch('show_edit_column_field', value => {
+    show = value
+    if (value) $dispatch('edit_column_id', [edit_column_id])
+});
+$watch('show', value => show_edit_column_field = value)">
+    <x-modal :name="'Jogosultság hozzáadás'">
+        <form wire:submit.prevent="save_edit_column">
+            <div class="sm:flex sm:items-start">
+                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3 class="text-base font-semibold text-gray-500 dark:text-gray-400" id="modal-title">
+                        Oszlop hozzáadás
+                    </h3>
+                    <div class="mt-2">
+                        <div class="grid grid-cols-4 gap-6">
+                            <div class="relative z-0 w-full mb-5 group">
+                                <input wire:model="edit_column_display_name" type="text"
+                                    name="edit_column_display_name" id="edit_column_display_name"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-indigo-600 focus:outline-none focus:ring-0 focus:border-indigo-400 peer"
+                                    placeholder=" " />
+                                <label for="edit_column_display_name"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-indigo-600 peer-focus:dark:text-indigo-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    Elnevezés
+                                </label>
+                                @error('edit_column_display_name')
+                                    <x-input-error :messages="$message" class="mt-2" />
+                                @enderror
+                            </div>
+
+                            <div class="relative z-0 w-full mb-5 group">
+                                <select wire:model="edit_column_status_id" type="select" name="edit_column_status_id"
+                                    id="edit_column_status_id"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-indigo-600 focus:outline-none focus:ring-0 focus:border-indigo-400 peer"
+                                    placeholder=" ">
+                                    @foreach ($this->statuses as $status)
+                                        <x-option value="{{ $status->id }}">{{ $status->displayName }}</x-option>
+                                    @endforeach
+                                </select>
+                                <label for="edit_column_status_id"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-indigo-600 peer-focus:dark:text-indigo-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    Státusz
+                                </label>
+                                @error('edit_column_status_id')
+                                    <x-input-error :messages="$message" class="mt-2" />
+                                @enderror
+                            </div>
+
+                            <div class="relative z-0 w-full mb-5 group">
+                                <select wire:model="edit_column_position" type="select" name="edit_column_position"
+                                    id="edit_column_position"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-indigo-600 focus:outline-none focus:ring-0 focus:border-indigo-400 peer"
+                                    placeholder=" ">
+                                    @for ($i = 1; $i <= $columns_number; $i++)
+                                        <x-option value="{{ $i }}">{{ $i }}</x-option>
+                                    @endfor
+                                </select>
+                                <label for="edit_column_position"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-indigo-600 peer-focus:dark:text-indigo-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    Pozíció
+                                </label>
+                                @error('edit_column_position')
+                                    <x-input-error :messages="$message" class="mt-2" />
+                                @enderror
+                            </div>
+                            <div x-data="{ showConfirmDelete: false }" class="relative z-0 w-full mb-5 group">
+                                <x-danger-button @click.prevent="showConfirmDelete = true">
+                                    {{ __('Törlés') }}
+                                </x-danger-button>
+                                <div x-show="showConfirmDelete">
+                                    <x-modal :name="'Jogosultság hozzáadás'">
+                                        <div class="m-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                            <h3 class="text-base font-semibold text-gray-500 dark:text-gray-400"
+                                                id="modal-title">
+                                                Biztosan törlöd?
+                                            </h3>
+                                            <x-success-button
+                                                @click.prevent="showConfirmDelete = false">Mégse</x-success-button>
+                                            <x-danger-button
+                                                @click.prevent="$wire.delete_edit_column(); showConfirmDelete = false">Igen,
+                                                törlöm</x-danger-button>
+                                        </div>
+                                    </x-modal>
+                                </div>
+                                @error('delete_edit_column')
+                                    <x-input-error :messages="$message" class="mt-2" />
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-gray-100 dark:bg-gray-600 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 items-center">
+                <x-danger-button @click.prevent="show_edit_column_field = !show_edit_column_field">
+                    {{ __('Bezárás') }}
+                </x-danger-button>
+
+                <x-success-button wire:click.prevent="save_edit_column" class="me-3">
+                    {{ __('Mentés') }}
+                </x-success-button>
+
+                <x-action-message wire:loading class="me-3" on="save_edit_column">
+                    {{ __('Betöltés') }}
+                </x-action-message>
+
+                <x-action-message-success class="me-3" on="edit_columns_save_success">
+                    {{ __('Sikeres mentés') }}
+                </x-action-message-success>
+
+                <x-action-message-success class="me-3" on="edit_columns_delete_success">
+                    {{-- ha ez meg jelenik: show = false --}}
+                    {{ __('Sikeres törlés') }}
+                </x-action-message-success>
+
+                @error('save_edit_column_error')
+                    <div class="me-3">
+                        <x-input-error :messages="$message" />
+                    </div>
+                @enderror
+            </div>
+        </form>
+    </x-modal>
+</div>
