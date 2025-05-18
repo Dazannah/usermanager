aa57883e7839c56a93bbde2f0fd4b8bf13aad556
Dazannah <beattius@gmail.com>
2025-05-18 16:23:19 +0200

    base files added

---
+++ app/Livewire/Admin/Components/EditSubAuthorization.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Components;
+
+use Livewire\Component;
+
+class EditSubAuthorization extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.components.edit-sub-authorization');
+    }
+}
