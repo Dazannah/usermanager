528224c9efc622dfafb162d4f14357f95d893631
Dazannah <beattius@gmail.com>
2025-05-22 18:58:16 +0200

    create location functionality added

---
+++ app/Livewire/Admin/Components/CreateLocation.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Livewire\Admin\Components;
+
+use App\Models\Location;
+use Exception;
+use Livewire\Component;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Validation\ValidationException;
+
+class CreateLocation extends Component {
+    /** @var Status $status */
+    public Collection $statuses;
+
+    //create properties
+    public $create_location_displayName, $create_location_note;
+    public $create_location_status_id = 1;
+
+    //create new
+    public function save_new_location() {
+        try {
+            //validation rules
+            $rules = [
+                'create_location_displayName' => 'required',
+                'create_location_status_id' => 'required'
+            ];
+
+            //vatidation messages
+            $messages = [
+                'create_location_displayName.required' => 'Elnevezés kitöltése kötelező',
+                'create_location_status_id.required' => 'Egy státuszt ki kell választani'
+            ];
+
+            $this->validate($rules, $messages);
+
+            //create and save location
+
+            $location = new Location([
+                'displayName' => $this->create_location_displayName,
+                'status_id' => $this->create_location_status_id,
+                'note' => empty($this->create_location_note) ? null : $this->create_location_note
+            ]);
+
+            $location->save();
+
+            //message and update $locations
+            $this->reset('create_location_displayName', 'create_location_note');
+
+            $this->dispatch('save_new_location_success');
+            $this->dispatch('refresh_locations');
+        } catch (ValidationException $err) {
+            throw $err;
+        } catch (Exception $err) {
+            $this->addError('save_new_location_error', $err->getMessage());
+
+            $this->dispatch('refresh_authorization_mount');
+        }
+    }
+
+    public function render() {
+        return view('livewire.admin.components.create-location');
+    }
+}
---
+++ resources/views/livewire/admin/components/create-location.blade.php
@@ -0,0 +1,83 @@
+<div x-data="{ show: false }" x-init="$watch('show_add_location_field', value => show = value);
+$watch('show', value => show_add_location_field = value)">
+    <x-modal :name="'Helyszín hozzáadás'">
+        <form wire:submit.prevent="save_new_location">
+            <div class="sm:flex sm:items-start">
+                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3 class="text-base font-semibold text-gray-500 dark:text-gray-400" id="modal-title">
+                        Helyszín hozzáadás
+                    </h3>
+                    <div class="mt-2">
+                        <div class="grid grid-cols-3 gap-6">
+                            <div class="relative z-0 w-full mb-5 group">
+                                <input wire:key="create_location_displayName" wire:model="create_location_displayName"
+                                    type="text" name="create_location_displayName" id="create_location_displayName"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-indigo-600 focus:outline-none focus:ring-0 focus:border-indigo-400 peer"
+                                    placeholder=" " />
+                                <label for="create_location_displayName"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-indigo-600 peer-focus:dark:text-indigo-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    Elnevezés
+                                </label>
+                                @error('create_location_displayName')
+                                    <x-input-error :messages="$message" class="mt-2" />
+                                @enderror
+                            </div>
+                            <div class="relative z-0 w-full mb-5 group">
+                                <select key:wire="create_location_status_id" wire:model="create_location_status_id"
+                                    type="select" name="create_location_status_id" id="create_location_status_id"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-indigo-600 focus:outline-none focus:ring-0 focus:border-indigo-400 peer"
+                                    placeholder=" ">
+                                    @foreach ($this->statuses as $status)
+                                        <x-option value="{{ $status->id }}">{{ $status->displayName }}</x-option>
+                                    @endforeach
+                                </select>
+                                <label for="create_location_status_id"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-indigo-600 peer-focus:dark:text-indigo-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    Státusz
+                                </label>
+                                @error('create_location_status_id')
+                                    <x-input-error :messages="$message" class="mt-2" />
+                                @enderror
+                            </div>
+                            <div class="relative z-0 w-full mb-5 group">
+                                <input wire:key="create_location_note" wire:model="create_location_note" type="text"
+                                    name="create_location_note" id="create_location_note"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-indigo-600 focus:outline-none focus:ring-0 focus:border-indigo-400 peer"
+                                    placeholder=" " />
+                                <label for="create_location_note"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-indigo-600 peer-focus:dark:text-indigo-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    Megjegyzés
+                                </label>
+                                @error('create_location_note')
+                                    <x-input-error :messages="$message" class="mt-2" />
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-gray-100 dark:bg-gray-600 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 items-center">
+                <x-danger-button @click.prevent="show_add_location_field = !show_add_location_field">{{ __('Bezárás') }}
+                </x-danger-button>
+
+                <x-success-button wire:click.prevent="save_new_location" class="me-3">
+                    {{ __('Mentés') }}
+                </x-success-button>
+
+                <x-action-message wire:loading class="me-3" on="save_new_location">
+                    {{ __('Betöltés') }}
+                </x-action-message>
+
+                <x-action-message-success class="me-3" on="save_new_location_success">
+                    {{ __('Sikeres mentés') }}
+                </x-action-message-success>
+
+                @error('save_new_location_error')
+                    <div class="me-3">
+                        <x-input-error :messages="$message" />
+                    </div>
+                @enderror
+            </div>
+        </form>
+    </x-modal>
+</div>
