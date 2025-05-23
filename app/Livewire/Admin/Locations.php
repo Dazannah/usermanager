4c095f8a1f796bfc72580ca25c3e117651502541
Dazannah <beattius@gmail.com>
2025-05-23 18:32:31 +0200

    added filter_locations and set properies to use url query string

--- app/Livewire/Admin/Locations.php
+++ app/Livewire/Admin/Locations.php
@@ -7,6 +7,7 @@ use App\Models\Location;
 use App\Models\Status;
 use Exception;
 use Illuminate\Database\Eloquent\Collection;
+use Livewire\Attributes\Url;
 
 class Locations extends Component {
     /** @var Collection<int,Location> $locations */
@@ -16,7 +17,12 @@ class Locations extends Component {
     public Collection $statuses;
 
     //filter properties
-    public $search_location_displayName, $search_location_status, $search_location_note;
+    #[Url(as: 'displayName')]
+    public string|null $search_location_displayName;
+    #[Url(as: 'note')]
+    public string|null $search_location_note;
+    #[Url(as: 'status_id')]
+    public int|null $search_location_status_id;
 
     public $listeners = ['refresh_locations_mount', 'location_filter_reset'];
 
@@ -25,16 +31,33 @@ class Locations extends Component {
     }
 
     public function location_filter_reset() {
-        $this->reset('search_location_displayName', 'search_location_status', 'search_location_note');
+        $this->reset('search_location_displayName', 'search_location_status_id', 'search_location_note');
+        $this->dispatch('refresh_locations_mount');
     }
 
     public function mount() {
-        $this->locations = Location::all();
+        $this->filter_locations();
         $this->statuses = Status::all();
     }
 
     //search
     public function filter_locations() {
+        $this->locations = Location::when(
+            isset($this->search_location_displayName) && !empty($this->search_location_displayName),
+            function ($query) {
+                return $query->where('displayName', 'REGEXP', $this->search_location_displayName);
+            }
+        )->when(
+            isset($this->search_location_status_id),
+            function ($query) {
+                return $query->where('status_id', '=', $this->search_location_status_id);
+            }
+        )->when(
+            isset($this->search_location_note) && !empty($this->search_location_note),
+            function ($query) {
+                return $query->where('note', 'REGEXP', $this->search_location_note);
+            }
+        )->get();
     }
 
     public function render() {
