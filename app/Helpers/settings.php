3df7e9f9334b0abfac0a7921500ada94f27e7860
Dazannah <beattius@gmail.com>
2025-05-24 18:19:02 +0200

    added MailSettings to auto load

--- app/Helpers/settings.php
+++ app/Helpers/settings.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Settings\AppSettings;
+use App\Settings\MailSettings;
 
 if (!function_exists('app_settings')) {
   function app_settings() {
     return app(AppSettings::class);
+  }
+}
+
+if (!function_exists('mail_settings')) {
+  function mail_settings() {
+    return app(MailSettings::class);
   }
 }
