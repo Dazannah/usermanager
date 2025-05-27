ed456e050a102eeab06ec7f97863174740a47e27
Dazannah <beattius@gmail.com>
2025-05-27 18:15:34 +0200

    create statuses on migrate

--- database/migrations/2025_05_03_000004_create_statuses_table.php
+++ database/migrations/2025_05_03_000004_create_statuses_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Status;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration {
     /**
@@ -14,6 +15,16 @@ return new class extends Migration {
             $table->text("name");
             $table->text("displayName");
         });
+
+        Status::create([
+            'name' => 'active',
+            'displayName' => 'Aktív'
+        ]);
+
+        Status::create([
+            'name' => 'inactive',
+            'displayName' => 'Inaktív'
+        ]);
     }
 
     /**
