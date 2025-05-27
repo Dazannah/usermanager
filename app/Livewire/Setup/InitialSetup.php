11b8f5b11aa1045b3cbf1af6563a323594168096
Dazannah <beattius@gmail.com>
2025-05-27 18:14:42 +0200

    init setup changed to create only builtin admin mode

--- app/Livewire/Setup/InitialSetup.php
+++ app/Livewire/Setup/InitialSetup.php
@@ -3,48 +3,20 @@
 namespace App\Livewire\Setup;
 
 use Exception;
-use SoapClient;
 use App\Models\User;
-use App\Mail\MailTest;
 use App\Models\Status;
 use Livewire\Component;
-use LdapRecord\Connection;
-use Illuminate\Validation\Rules;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Artisan;
 
 class InitialSetup extends Component {
-    //todo feltördelni, külön külön menthetővé tenni, nehezen kiegészíthető
-
-    public $db_host;
-    public $db_port;
-    public $db_databasename;
-    public $db_username;
-    public $db_password;
-
     public $admin_name;
     public $admin_username;
     public $admin_email;
     public $password;
     public $password_confirmation;
 
-    public $ispfonfig_active;
-    public $ispconfig_soap_uri;
-    public $ispconfig_soap_location;
-    public $ispconfig_soap_remote_username;
-    public $ispconfig_soap_remote_user_password;
-
-    private $user_principal_name;
-
     protected $rules = [
-        'db_host' => 'required',
-        'db_port' => 'required|integer|min:1|max:65535',
-        'db_databasename' => 'required',
-        'db_username' => 'required',
-        'db_password' => 'required',
         'admin_name' => 'max:255',
         'admin_username' => 'required|max:255',
         'admin_email' => 'required|email|max:255',
@@ -52,14 +24,6 @@ class InitialSetup extends Component {
     ];
 
     protected $messages = [
-        'db_host.required' => 'Adatbázis szerver cím megadása kötelező.',
-        'db_port.required' => 'Adatbázis szerver port megadása kötelező.',
-        'db_port.integer' => 'Az adatbázis portnak egész számnak kell lennie.',
-        'db_port.min' => 'Az adatbázis port minimum 1 lehet.',
-        'db_port.max' => 'Az adatbázis port maximum 65535 lehet.',
-        'db_databasename.required' => 'Adatbázis név megadása kötelező.',
-        'db_username.required' => 'Adatbázis felhasználó név megadása kötelező.',
-        'db_password.required' => 'Adatbázis felhasználó jelszó cím megadása kötelező.',
         'admin_username.required' => 'Admin felhasználónév megadása kötelező.',
         'admin_username.max' => 'Admin felhasználónév túl hosszú.',
         'admin_email.required' => 'Admin email cím megadása kötelező.',
@@ -68,155 +32,23 @@ class InitialSetup extends Component {
         'password.confirmed' => 'Megadott jelszavak nem egyeznek.',
     ];
 
-    public function mount() {
-        $this->db_host = config('database.connections.mysql.host');
-        $this->db_port = config('database.connections.mysql.port');
-        $this->db_databasename = config('database.connections.mysql.database');
-        $this->db_username = config('database.connections.mysql.username');
-        $this->db_password = config('database.connections.mysql.password');
-
-        if (!config('app.installed')) {
-            $this->admin_name = '';
-            $this->admin_username = 'usermanager_admin';
-            $this->admin_email = '';
-            $this->password = '';
-            $this->password_confirmation = '';
-        }
-
-        $this->ispfonfig_active = config('ispconfig.soap.active');
-        $this->ispconfig_soap_uri = config('ispconfig.soap.connection.uri');
-        $this->ispconfig_soap_location = config('ispconfig.soap.connection.location');
-        $this->ispconfig_soap_remote_username = config('ispconfig.soap.connection.username');
-        $this->ispconfig_soap_remote_user_password = config('ispconfig.soap.connection.password');
-    }
-
-    public function updated($propertyName) {
-        $this->validateOnly($propertyName);
-    }
-
-    public function validate_only_array($field_names) {
-        foreach ($field_names as $field_name) {
-            $this->validate($field_name);
-        }
-    }
-
-    public function test_database_connection() {
-        try {
-            config([
-                'database.connections.mysql.host' => $this->db_host,
-                'database.connections.mysql.port' => $this->db_port,
-                'database.connections.mysql.database' => $this->db_databasename,
-                'database.connections.mysql.username' => $this->db_username,
-                'database.connections.mysql.password' => $this->db_password
-            ]);
-
-            DB::connection()->getPDO();
-            DB::disconnect();
-
-            return true;
-        } catch (Exception $err) {
-            return $err->getMessage();
-        }
-    }
-
-    public function test_database_connection_standalone() {
-        $result = $this->test_database_connection();
-
-        if ($result === true) {
-            session()->flash('db_test_result', "Sikeres kapcsolat kiépítés.");
-
-            return;
-        } else {
-            $this->addError('db_test_result_error', $result);
-
-            return;
-        }
-    }
-
-
-
-    public function save() {
-        $this->validate();
-        $this->validate(['password' => [Rules\Password::defaults()]]);
-
-
-        //mail setup test before save
-        $mail_test_result = $this->test_mail_connection();
-
-        if ($mail_test_result !== true) {
-            $this->addError('mail_test_result_error', $mail_test_result);
-
-            return;
-        }
-
-        //database setup test before save
-        $db_test_result = $this->test_database_connection();
-
-        if ($db_test_result !== true) {
-            $this->addError('db_test_result_error', $db_test_result);
-
-            return;
-        }
+    public function save_admin() {
+        $this->validate($this->rules, $this->messages);
 
         //sikeres validáció után .env módosítása és mentése
         $env_content = $original_env_content = file_get_contents(base_path('.env'));
-        $original_database_connctions = config('database.connctions');
 
         try {
-            //general configs
-            $env_content = preg_replace('/APP_NAME=.*/', "APP_NAME='$this->app_name'", $env_content);
-            $env_content = preg_replace('/SESSION_DRIVER=.*/', "SESSION_DRIVER=database", $env_content);
-
-            config([
-                'app.name' => $this->app_name,
-                'session.driver' => 'database',
-            ]);
-
-            //database configs
-            $env_content = preg_replace('/DB_HOST=.*/', "DB_HOST='$this->db_host'", $env_content);
-            $env_content = preg_replace('/DB_PORT=.*/', "DB_PORT='$this->db_port'", $env_content);
-            $env_content = preg_replace('/DB_DATABASE=.*/', "DB_DATABASE='$this->db_databasename'", $env_content);
-            $env_content = preg_replace('/DB_USERNAME=.*/', "DB_USERNAME='$this->db_username'", $env_content);
-            $env_content = preg_replace('/DB_PASSWORD=.*/', "DB_PASSWORD='$this->db_password'", $env_content);
-
-            config([
-                'database.connections.mysql.host' => $this->db_host,
-                'database.connections.mysql.port' => $this->db_port,
-                'database.connections.mysql.database' => $this->db_databasename,
-                'database.connections.mysql.username' => $this->db_username,
-                'database.connections.mysql.password' => $this->db_password
-            ]);
-
-            //adatbázis migráció
-            Artisan::call('migrate', array(
-                '--path' => 'database/migrations',
-                '--database' => 'mysql',
-                '--force' => true
-            ));
-
-            Status::create([
-                'name' => 'active',
-                'displayName' => 'Aktív'
-            ]);
-
-            Status::create([
-                'name' => 'inactive',
-                'displayName' => 'Inaktív'
-            ]);
-
             //alapértelmezett rendszergazda fiók létrehozása
-            if (!config('app.installed')) {
+            $user = User::create([
+                'name' => $this->admin_name ?? $this->admin_username,
+                'username' => $this->admin_username,
+                'email' => $this->admin_email,
+                'password' => Hash::make($this->password),
+                'is_admin' => true
+            ]);
 
-                $user_data['name'] = $this->admin_name | $this->admin_username;
-                $user_data['username'] = $this->admin_username;
-                $user_data['email'] = $this->admin_email;
-                $user_data['password'] = Hash::make($this->password);
-                $user_data['is_admin'] = true;
-
-                $user = User::create($user_data);
-
-                Auth::login($user);
-            }
+            Auth::login($user);
 
             $env_content = preg_replace('/APP_INSTALLED=.*/', "APP_INSTALLED=true", $env_content);
 
@@ -229,6 +61,8 @@ class InitialSetup extends Component {
                 $env_content
             );
 
+            $this->dispatch('save_admin_success');
+
             return $this->redirect('/', navigate: true);
         } catch (Exception $err) {
             $this->addError('save_error', $err->getMessage());
@@ -238,13 +72,6 @@ class InitialSetup extends Component {
                 base_path('.env'),
                 $original_env_content
             );
-
-            //function elején kimentett konf vissza töltése
-            config([
-                'session.driver' => 'file',
-                'app.installed' => false,
-                'database.connections' => $original_database_connctions
-            ]);
         }
     }
 
--- resources/views/livewire/setup/initial-setup.blade.php
+++ resources/views/livewire/setup/initial-setup.blade.php
@@ -14,112 +14,54 @@
                 @endif
             </div>
         </header>
-        <form wire:submit.prevent="save" class="mt-6 space-y-6">
-
-
-
+        <form class="mt-6 space-y-6">
             <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                {{ __('Adatbázis beállítások MYSQL') }}
+                {{ __('Alapértelmezett rendszergazda fiók') }}
             </h2>
-            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                {{ __('Adatbázist előre létre kell hozni.') }}
-            </p>
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    <x-input-label for="db_host" :value="__('Szerver címe')" />
-                    <x-text-input placeholder="Szerver címe" wire:model.live="db_host" id="db_host" name="db_host"
+                    <x-input-label for="admin_name" :value="__('Név')" />
+                    <x-text-input placeholder="Név" wire:model.live="admin_name" id="admin_name" name="admin_name"
                         type="text" class="mt-1 block w-full" />
-                    @error('db_host')
+                    @error('admin_name')
                         <x-input-error :messages="$message" class="mt-2" />
                     @enderror
 
-                    <x-input-label for="db_port" :value="__('Port')" />
-                    <x-text-input placeholder="3306" wire:model.live="db_port" id="db_port" name="db_port"
-                        type="number" class="mt-1 block w-full" />
-                    @error('db_port')
+                    <x-input-label for="admin_username" :value="__('Felhasználónév')" />
+                    <x-text-input placeholder="Felhasználónév" wire:model.live="admin_username" id="admin_username"
+                        name="admin_username" type="text" class="mt-1 block w-full" />
+                    @error('admin_username')
                         <x-input-error :messages="$message" class="mt-2" />
                     @enderror
 
-                    <x-input-label for="db_databasename" :value="__('Adatbázis név')" />
-                    <x-text-input placeholder="Adatbázis név" wire:model.live="db_databasename" id="db_databasename"
-                        name="db_databasename" type="text" class="mt-1 block w-full" />
-                    @error('db_databasename')
+                    <x-input-label for="admin_email" :value="__('Email cím')" />
+                    <x-text-input placeholder="Email cím" wire:model.live="admin_email" id="admin_email"
+                        name="admin_email" type="email" class="mt-1 block w-full" />
+                    @error('admin_email')
                         <x-input-error :messages="$message" class="mt-2" />
                     @enderror
 
-                    <x-input-label for="db_username" :value="__('Felhasználó név')" />
-                    <x-text-input placeholder="Felhasználó név" wire:model.live="db_username" id="db_username"
-                        name="db_username" type="text" class="mt-1 block w-full" />
-                    @error('db_username')
-                        <x-input-error :messages="$message" class="mt-2" />
-                    @enderror
-
-                    <x-input-label for="db_password" :value="__('Jelszó')" />
-                    <x-text-input placeholder="Jelszó" wire:model.live="db_password" id="db_password" name="db_password"
+                    <x-input-label for="password" :value="__('Jelszó')" />
+                    <x-text-input placeholder="Jelszó" wire:model.live="password" id="password" name="password"
                         type="password" class="mt-1 block w-full" />
-                    @error('db_password')
+                    @error('password')
                         <x-input-error :messages="$message" class="mt-2" />
                     @enderror
-                    @error('db_test_result_error')
+
+                    <x-input-label for="password_confirmation" :value="__('Jelszó megerősítése')" />
+                    <x-text-input placeholder="Jelszó megerősítése" wire:model.live="password_confirmation"
+                        id="password_confirmation" name="password_confirmation" type="password"
+                        class="mt-1 block w-full" />
+                    @error('password_confirmation')
                         <x-input-error :messages="$message" class="mt-2" />
                     @enderror
-                    @if (session()->has('db_test_result'))
-                        <div class="alert alert-success">
-                            {{ session('db_test_result') }}
-                        </div>
-                    @endif
-                    <x-primary-button
-                        wire:click.prevent="test_database_connection_standalone">{{ __('Kapcsolat tesztelése') }}</x-primary-button>
-                    <x-action-message wire:loading class="me-3" on="test_database_connection_standalone">
-                        {{ __('Betöltés') }}
-                    </x-action-message>
                 </div>
             </div>
-            @if (!config('app.installed'))
-                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                    {{ __('Alapértelmezett rendszergazda fiók') }}
-                </h2>
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                    <div class="max-w-xl">
-                        <x-input-label for="admin_name" :value="__('Név')" />
-                        <x-text-input placeholder="Név" wire:model.live="admin_name" id="admin_name" name="admin_name"
-                            type="text" class="mt-1 block w-full" />
-                        @error('admin_name')
-                            <x-input-error :messages="$message" class="mt-2" />
-                        @enderror
-
-                        <x-input-label for="admin_username" :value="__('Felhasználónév')" />
-                        <x-text-input placeholder="Felhasználónév" wire:model.live="admin_username" id="admin_username"
-                            name="admin_username" type="text" class="mt-1 block w-full" />
-                        @error('admin_username')
-                            <x-input-error :messages="$message" class="mt-2" />
-                        @enderror
-
-                        <x-input-label for="admin_email" :value="__('Email cím')" />
-                        <x-text-input placeholder="Email cím" wire:model.live="admin_email" id="admin_email"
-                            name="admin_email" type="email" class="mt-1 block w-full" />
-                        @error('admin_email')
-                            <x-input-error :messages="$message" class="mt-2" />
-                        @enderror
-
-                        <x-input-label for="password" :value="__('Jelszó')" />
-                        <x-text-input placeholder="Jelszó" wire:model.live="password" id="password" name="password"
-                            type="password" class="mt-1 block w-full" />
-                        @error('password')
-                            <x-input-error :messages="$message" class="mt-2" />
-                        @enderror
-
-                        <x-input-label for="password_confirmation" :value="__('Jelszó megerősítése')" />
-                        <x-text-input placeholder="Jelszó megerősítése" wire:model.live="password_confirmation"
-                            id="password_confirmation" name="password_confirmation" type="password"
-                            class="mt-1 block w-full" />
-                        @error('password_confirmation')
-                            <x-input-error :messages="$message" class="mt-2" />
-                        @enderror
-                    </div>
-                </div>
-            @endif
-
-
+            <div class="flex items-center gap-4">
+                <x-primary-button wire:click.prevent="save_admin">{{ __('Mentés') }}</x-primary-button>
+                <x-action-message-success class="me-3" on="save_admin_success">
+                    {{ __('Sikeres mentés') }}
+                </x-action-message-success>
+            </div>
         </form>
 </div>
