b3f270c9a9a28256029ed6f7f0f116dd607eb2e8
Dazannah <beattius@gmail.com>
2025-05-29 21:13:56 +0200

    updated to uniform layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,7 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Személyes összefoglaló') }}
-        </h2>
-    </x-slot>
+
+    <x-submenu :title="'Személyes összefoglaló'">
+    </x-submenu>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,19 +28,18 @@
                 </div>
             </header>
         @endif
-        <div class="py-4">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                @session('error')
+
+        @session('error')
+            <div class="py-4">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <x-input-error :messages="$value" class="mt-2" />
-                @endsession
+                </div>
             </div>
-        </div>
+        @endsession
 
         <!-- Page Content -->
-        <main class="py-4">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                {{ $slot }}
-            </div>
+        <main>
+            {{ $slot }}
         </main>
     </div>
 </body>
