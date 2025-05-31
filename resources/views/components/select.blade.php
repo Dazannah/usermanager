da919b86b4b08eff934044b24991bfdcc955007c
Dazannah <beattius@gmail.com>
2025-05-31 21:20:18 +0200

    padding added

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -15,7 +15,7 @@
 
 <select @disabled($disabled) wire:key="{{ $key }}" wire:model="{{ $property_name }}" type="select"
     name="{{ $property_name }}" id="{{ $property_name }}"
-    {{ $attributes->merge(['class' => 'block py-2.5 px-0 w-full text-sm text-[#15808a] focus:text-[#e3a420] bg-transparent border-0 border-b-2 border-[#15808a] appearance-none dark:text-[#15808a] focus:dark:text-[#e3a420] dark:border-[#15808a] dark:focus:border-[#e3a420] focus:outline-none focus:ring-0 focus:border-[#e3a420] peer']) }}placeholder=" ">
+    {{ $attributes->merge(['class' => 'block p-2.5 w-full text-sm text-[#15808a] focus:text-[#e3a420] bg-transparent border-0 border-b-2 border-[#15808a] appearance-none dark:text-[#15808a] focus:dark:text-[#e3a420] dark:border-[#15808a] dark:focus:border-[#e3a420] focus:outline-none focus:ring-0 focus:border-[#e3a420] peer']) }}placeholder=" ">
 
     @if ($select)
         <x-option value="">{{ $select_value }}</x-option>
