cfa0a2118d8ad1660c6aebc9fb9bfb263254153c
Dazannah <beattius@gmail.com>
2025-06-01 15:36:15 +0200

    auth level deleted

--- database/migrations/2025_05_31_152607_create_local_account_authorization_levels_table.php
+++ database/migrations/2025_05_31_152607_create_local_account_authorization_levels_table.php
@@ -12,17 +12,16 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('account_authorization_levels', function (Blueprint $table) {
             $table->id();
-            $table->string('technicalName');
+            $table->string('name');
             $table->string('displayName');
             $table->string('ldap_group_name');
-            $table->integer('auth_level');
         });
 
-        AccountAuthorizationLevel::create(['technicalName' => 'base', 'displayName' => 'alap', 'ldap_group_name' => 'JogosultsagigenyAlap', 'auth_level' => 1]);
-        AccountAuthorizationLevel::create(['technicalName' => 'authorizer', 'displayName' => 'engedélyező', 'ldap_group_name' => 'JogosultsagigenyEngedelyezok', 'auth_level' => 2]);
-        AccountAuthorizationLevel::create(['technicalName' => 'reqAdmin', 'displayName' => 'adminisztrátor', 'ldap_group_name' => 'JogosultsagigenyAdminisztrator', 'auth_level' => 4]);
-        AccountAuthorizationLevel::create(['technicalName' => 'dlHandler', 'displayName' => 'terjesztési lista kezelő', 'ldap_group_name' => 'JogosultsagigenyTerjesztesilista', 'auth_level' => 8]);
-        AccountAuthorizationLevel::create(['technicalName' => 'sysAdmin', 'displayName' => 'rendszergazda', 'ldap_group_name' => 'JogosultsagigenyRendszergazda', 'auth_level' => 31]);
+        AccountAuthorizationLevel::create(['name' => 'base', 'displayName' => 'alap', 'ldap_group_name' => 'JogosultsagigenyAlap']);
+        AccountAuthorizationLevel::create(['name' => 'authorizer', 'displayName' => 'engedélyező', 'ldap_group_name' => 'JogosultsagigenyEngedelyezok']);
+        AccountAuthorizationLevel::create(['name' => 'reqAdmin', 'displayName' => 'adminisztrátor', 'ldap_group_name' => 'JogosultsagigenyAdminisztrator']);
+        AccountAuthorizationLevel::create(['name' => 'dlHandler', 'displayName' => 'terjesztési lista kezelő', 'ldap_group_name' => 'JogosultsagigenyTerjesztesilista']);
+        AccountAuthorizationLevel::create(['name' => 'sysAdmin', 'displayName' => 'rendszergazda', 'ldap_group_name' => 'JogosultsagigenyRendszergazda']);
     }
 
     /**
