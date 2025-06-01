7d729221bf56b8dca00456fa503f80021cfb71fa
Dazannah <beattius@gmail.com>
2025-06-01 15:38:26 +0200

    can login use now AuthorizationLevelService if it have any auth

--- app/Ldap/Rules/CanLogin.php
+++ app/Ldap/Rules/CanLogin.php
@@ -4,6 +4,7 @@ namespace App\Ldap\Rules;
 
 use LdapRecord\Laravel\Auth\Rule;
 use LdapRecord\Models\Model as LdapRecord;
+use App\Services\AuthorizationLevelService;
 use LdapRecord\Models\ActiveDirectory\Group;
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
@@ -11,13 +12,9 @@ class CanLogin implements Rule {
     /**
      * Check if the rule passes validation.
      */
-    public function passes(LdapRecord $user, Eloquent $model = null): bool { // make it configurable
-        $base_auths = Group::where('cn', '=', 'JogosultsagigenyAlap')->first();
-        $permission_auths = Group::where('cn', '=', 'JogosultsagigenyEngedelyezok')->first();
-        $distribution_list_auths = Group::where('cn', '=', 'JogosultsagigenyTerjesztesilista')->first();
-        $admin_auths = Group::where('cn', '=', 'JogosultsagigenyAdminisztrator')->first();
+    public function passes(LdapRecord $user, Eloquent|null $model = null): bool { // make it configurable
+        $user_auth_level = AuthorizationLevelService::get_ldap_user_auth_level($model->username, $user);
 
-        $result = $user->groups()->recursive()->exists($base_auths) || $user->groups()->recursive()->exists($permission_auths) || $user->groups()->recursive()->exists($distribution_list_auths) || $user->groups()->recursive()->exists($admin_auths);
-        return $result;
+        return $user_auth_level  > 0;
     }
 }
