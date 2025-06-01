502825674db1d9581224e377ce6127dfbb9ec17d
Dazannah <beattius@gmail.com>
2025-06-01 15:37:05 +0200

    updated with some function, changed the handle of ldap users

--- app/Services/AuthorizationLevelService.php
+++ app/Services/AuthorizationLevelService.php
@@ -3,76 +3,120 @@
 namespace App\Services;
 
 use App\Models\User;
+use LdapRecord\Models\Model as LdapRecord;
 use App\Models\AccountAuthorizationLevel;
 use LdapRecord\Models\ActiveDirectory\User as LdapUser;
 
 class AuthorizationLevelService {
-  public const BASE = 0b00001;
-  public const BASE_LDAP = 'base';
-  public const AUTHORIZER = 0b00010;
-  public const AUTHORIZER_LDAP = 'authorizer';
-  public const REQ_ADMIN = 0b00100;
-  public const REQ_ADMIN_LDAP = 'reqAdmin';
-  public const DL_HANDLER = 0b01000;
-  public const DL_HANDLER_LDAP = 'dlHandler';
-  public const SYS_ADMIN = 0b10000;
-  public const SYS_ADMIN_LDAP = 'sysAdmin';
 
-  public static function hasLevel(User $user, int $level, string $ldapName): bool {
-    if ($user->is_local) {
-      return self::calculate_auth_bin($user->auth_level->auth_level, $level);
+  public const AUTH_LEVELS = [
+    'base' => 0b00001,
+    'authorizer' => 0b00010,
+    'reqAdmin' => 0b00100,
+    'dlHandler' => 0b01000,
+    'sysAdmin' => 0b10000,
+  ];
+
+  public static function get_sys_admin_level() {
+    $user_level = 0;
+
+    foreach (self::AUTH_LEVELS as $auth_level) {
+      $user_level |= $auth_level;
     }
 
-    if (config('ldap.active')) {
-      return self::is_ldap_have_auth($ldapName, $user->username);
-    }
+    return $user_level;
+  }
 
-    return false;
+  public static function hasLevel(User $user, int $level): bool {
+    $user_auth_level = self::get_user_auth_level($user);
+
+    return self::calculate_auth_bin($user_auth_level, $level);
   }
 
   // create new requests, search users and tickets
   public static function is_base(User $user): bool {
-    return self::hasLevel($user, self::BASE, self::BASE_LDAP);
+    return self::hasLevel($user, self::AUTH_LEVELS['base']);
   }
 
   // allow or deny requests
   public static function is_authorizer(User $user): bool {
-    return self::hasLevel($user, self::AUTHORIZER, self::AUTHORIZER_LDAP);
+    return self::hasLevel($user, self::AUTH_LEVELS['authorizer']);
   }
 
   // close requests
   public static function is_req_admin(User $user): bool {
-    return self::hasLevel($user, self::REQ_ADMIN, self::REQ_ADMIN_LDAP);
+    return self::hasLevel($user, self::AUTH_LEVELS['reqAdmin']);
   }
 
   // create, edit and delete distribution lists
   public static function is_dl_handler(User $user): bool {
-    return self::hasLevel($user, self::DL_HANDLER, self::DL_HANDLER_LDAP);
+    return self::hasLevel($user, self::AUTH_LEVELS['dlHandler']);
   }
 
   // access admin pages
   public static function is_sys_admin(User $user): bool {
-    return self::hasLevel($user, self::SYS_ADMIN, self::SYS_ADMIN_LDAP);
+    return self::hasLevel($user, self::AUTH_LEVELS['sysAdmin']);
+  }
+
+  public static function get_auth_level_names($user) {
+    $accountAuthorizationLevels = AccountAuthorizationLevel::all();
+
+    if (self::is_sys_admin($user))
+      foreach ($accountAuthorizationLevels as $accountAuthorizationLevel) {
+        if ($accountAuthorizationLevel->name === 'sysAdmin') {
+          return [$accountAuthorizationLevel->displayName];
+        }
+      }
+
+    $user_auth_level_names = [];
+
+    $user_auth_level = self::get_user_auth_level($user);
+
+    foreach (self::AUTH_LEVELS as $key => $auth_level) {
+      if (($user_auth_level & $auth_level) == $auth_level) {
+        foreach ($accountAuthorizationLevels as $accountAuthorizationLevel) {
+          if ($accountAuthorizationLevel->name == $key)
+            array_push($user_auth_level_names, $accountAuthorizationLevel->displayName);
+        }
+      }
+    }
+
+    return $user_auth_level_names;
+  }
+
+  public static function get_user_auth_level($user) {
+    $user_auth_level = 0;
+
+    if ($user->is_local) {
+      $user_auth_level = $user->auth_level;
+    } elseif (config('ldap.active')) {
+      $user_auth_level = self::get_ldap_user_auth_level($user->username);
+    }
+
+    return $user_auth_level;
   }
 
   protected static function calculate_auth_bin($user_auth_level, $needed_level) {
     return ($user_auth_level & $needed_level) === $needed_level;
   }
 
-  protected static function is_ldap_have_auth($needed_level, $username) {
-    $accountAuthorizationLevelAdmin = AccountAuthorizationLevel::where('technicalName', $needed_level)->firstOrFail();
+  public static function get_ldap_user_auth_level($username, LdapRecord|null $user = null) {
+    $user_auth_level = 0;
 
-    $user = LdapUser::where('samaccountname', '=', $username)->first();
+    $accountAuthorizationLevels = AccountAuthorizationLevel::all();
+
+    if (!isset($user))
+      $user = LdapUser::where('samaccountname', '=', $username)->first();
 
     $user_ldap_groups = $user->groups()->recursive()->get()->map(function ($group) {
       return $group->getName();
     })->all();
 
-    foreach ($user_ldap_groups as $user_ldap_group) {
-      if ($user_ldap_group === $accountAuthorizationLevelAdmin->ldap_group_name)
-        return true;
+    foreach ($accountAuthorizationLevels as $accountAuthorizationLevel) {
+      if (in_array($accountAuthorizationLevel->ldap_group_name, $user_ldap_groups))
+        $user_auth_level |= self::AUTH_LEVELS[$accountAuthorizationLevel->name];
     }
 
-    return false;
+    return $user_auth_level;
   }
 }
