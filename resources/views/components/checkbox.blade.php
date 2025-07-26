714102385901c59c444f1f55e7b154bc6ce75127
Dazannah <beattius@gmail.com>
2025-07-26 10:57:05 +0200

    debounce deleted

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -2,8 +2,8 @@
 
 <div>
     <input @disabled($disabled) @if ($on_click) @click="{{ $on_click }}" @endif
-        wire:key="{{ $wire_key }}" wire:model.live.debounce.500ms="{{ $property_name }}" id="{{ $property_name }}"
-        type="checkbox" wire:checked="{{ $property_name }}"
+        wire:key="{{ $wire_key }}" wire:model.live="{{ $property_name }}" id="{{ $property_name }}" type="checkbox"
+        wire:checked="{{ $property_name }}"
         class="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-[#15808a] dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-[#15808a] dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
 
     <label for="{{ $property_name }}"
