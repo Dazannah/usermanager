1dc82586b5cceefffa9d9d5f1a6f25e3188499aa
Dazannah <beattius@gmail.com>
2025-07-27 18:25:59 +0200

    DepartmentManagerModal implemented

--- app/Livewire/Admin/Components/DepartmentManagerModal.php
+++ app/Livewire/Admin/Components/DepartmentManagerModal.php
@@ -3,12 +3,57 @@
 namespace App\Livewire\Admin\Components;
 
 use Livewire\Component;
-use Illuminate\Database\Eloquent\Collection;
+use Livewire\WithPagination;
+use App\Models\DepartmentManager;
+use Livewire\WithoutUrlPagination;
 
 class DepartmentManagerModal extends Component {
+    use WithPagination, WithoutUrlPagination;
+
+    public string $search_department_manager_displayName;
+    public string $search_department_manager_registration_number;
+    public string $search_department_manager_note;
+
+    public function getPageName() {
+        return 'manager_page';
+    }
+
+    public  $listeners = ['filter_department_managers', 'department_manager_filter_reset', 'refresh_department_managers_mount'];
+
+    public function mount() {
+    }
+
+    public function refresh_department_managers_mount() {
+        $this->mount();
+    }
+
+    public function department_manager_filter_reset() {
+        $this->reset('search_department_manager_displayName', 'search_department_manager_registration_number', 'search_department_manager_note');
+        $this->resetPage();
+        $this->dispatch('refresh_department_managers_mount');
+    }
 
     public function filter_department_managers() {
-        return [];
+        return DepartmentManager::when(
+            isset($this->search_department_manager_displayName) && !empty($this->search_department_manager_displayName),
+            function ($query) {
+                return $query->whereHas('worker', function ($inside_query) {
+                    $inside_query->where('name', 'LIKE', "%$this->search_department_manager_displayName%");
+                });
+            }
+        )->when(
+            isset($this->search_department_manager_registration_number) && !empty($this->search_department_manager_registration_number),
+            function ($query) {
+                return $query->whereHas('worker', function ($inside_query) {
+                    $inside_query->where('registration_number', 'LIKE', "%$this->search_department_manager_registration_number%");
+                });
+            }
+        )->when(
+            isset($this->search_department_manager_note) && !empty($this->search_department_manager_note),
+            function ($query) {
+                return $query->where('note', 'LIKE', "%$this->search_department_manager_note%");
+            }
+        )->paginate(10);
     }
 
     public function render() {
