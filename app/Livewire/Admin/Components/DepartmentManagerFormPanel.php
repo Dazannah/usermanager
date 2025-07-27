86cd071a8645026ce2fde91badb554aea40c72a0
Dazannah <beattius@gmail.com>
2025-07-27 18:22:52 +0200

    DepartmentManagerFormPanel base created

---
+++ app/Livewire/Admin/Components/DepartmentManagerFormPanel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Livewire\Admin\Components;
+
+use Livewire\Component;
+
+class DepartmentManagerFormPanel extends Component {
+    public function render() {
+        return view('livewire.admin.components.department-manager-form-panel');
+    }
+}
