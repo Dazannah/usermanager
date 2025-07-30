7a7a76999c01f34e43f5d73da0127b3b659a1a19
Dazannah <beattius@gmail.com>
2025-07-30 20:38:05 +0200

    added crud, also dont retrive on create already created

--- app/Livewire/Admin/Components/DepartmentManagerFormPanel.php
+++ app/Livewire/Admin/Components/DepartmentManagerFormPanel.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire\Admin\Components;
 
+use Exception;
 use App\Models\Worker;
 use Livewire\Component;
 use Livewire\WithPagination;
 use Livewire\WithoutUrlPagination;
+use Illuminate\Database\QueryException;
 use Illuminate\Database\Eloquent\Collection;
 use App\Livewire\Forms\DepartmentManagerForm;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class DepartmentManagerFormPanel extends Component {
 
@@ -21,7 +25,16 @@ class DepartmentManagerFormPanel extends Component {
     public string|null $worker_name_search;
     public string|null $registration_number_search;
 
-    public $listeners = ['worker_filter_reset', 'set_worker'];
+    public $listeners = ['worker_filter_reset', 'set_worker', 'update_department_manager_id', 'show_store_department_manager'];
+
+    public function update_department_manager_id($department_manager_id) {
+        $this->form->set_department_manager($department_manager_id);
+    }
+
+    public function worker_filter_reset() {
+        $this->reset('worker_name_search', 'registration_number_search');
+        $this->resetPage();
+    }
 
     public function set_worker($id) {
         $worker = Worker::findOrFail($id);
@@ -31,13 +44,64 @@ class DepartmentManagerFormPanel extends Component {
         $this->form->worker_id = $worker->id;
     }
 
-    public function worker_filter_reset() {
-        $this->reset('worker_name_search', 'registration_number_search');
-        $this->resetPage();
+    public function show_store_department_manager() {
+        $this->form->reset();
+    }
+
+    public function delete_department_manager() {
+        try {
+            $this->form->delete();
+
+            $this->dispatch('refresh_departments_mount');
+            $this->dispatch('refresh_department_managers_mount');
+            $this->dispatch('department_manager_delete_success');
+        } catch (QueryException $err) {
+            $err_message = $err->getMessage();
+
+            if ($err->getCode() == 23000) {
+                $this->addError('department_manager_delete_error', "Nem lehet törölni az osztálytvezető, mert valószínűleg kapcsolódik más adatokhoz (pl. osztályhoz van rendelve ).");
+
+                return;
+            }
+
+            $this->addError('department_manager_delete_error', "Ismeretlen hiba történt: $err_message");
+        } catch (ValidationException $err) {
+            throw $err;
+        } catch (Exception $err) {
+            $this->addError('department_manager_delete_error', $err->getMessage());
+        }
+    }
+
+    public function update_department_manager() {
+        try {
+            $this->form->update();
+
+            $this->dispatch('refresh_departments_mount');
+            $this->dispatch('refresh_department_managers_mount');
+            $this->dispatch('department_manager_save_success');
+        } catch (ValidationException $err) {
+            throw $err;
+        } catch (Exception $err) {
+            $this->addError('department_manager_delete_error', $err->getMessage());
+        }
+    }
+
+    public function create_department_manager() {
+        try {
+            $this->form->create();
+
+            $this->dispatch('refresh_departments_mount');
+            $this->dispatch('refresh_department_managers_mount');
+            $this->dispatch('department_manager_save_success');
+        } catch (ValidationException $err) {
+            throw $err;
+        } catch (Exception $err) {
+            $this->addError('department_manager_delete_error', $err->getMessage());
+        }
     }
 
     public function filter_workers() {
-        return Worker::where('status_id', '=', '1')->where('is_technical', '=', '0')->when(
+        return Worker::where('status_id', '=', '1')->where('is_technical', '=', '0')->doesntHave('department_manager')->when(
             isset($this->worker_name_search) && !empty($this->worker_name_search),
             function ($query) {
                 return $query->where('name', 'LIKE', "%$this->worker_name_search%");
--- app/Livewire/Forms/DepartmentManagerForm.php
+++ app/Livewire/Forms/DepartmentManagerForm.php
@@ -3,13 +3,91 @@
 namespace App\Livewire\Forms;
 
 use Livewire\Form;
+use App\Models\Worker;
+use App\Models\Department;
+use Illuminate\Validation\Rule;
+use App\Models\DepartmentManager;
 use Livewire\Attributes\Validate;
+use App\Livewire\Admin\Departments;
+use Illuminate\Validation\ValidationException;
 
 class DepartmentManagerForm extends Form {
     public int|null $worker_id;
     public int|null $status_id = 1;
-    public string|null $note;
 
     public string|null $worker_name;
     public string|null $registration_number;
+
+    public $update_rules = [
+        'worker_id' => 'required',
+        'status_id' => 'required'
+    ];
+
+    public function rules() {
+        return [
+            'worker_id' => [
+                'required',
+                Rule::unique('department_managers', 'worker_id')
+            ],
+            'status_id' => 'required',
+        ];
+    }
+
+    public function messages() {
+        return [
+            'worker_id.required' => 'Dolgozó megadása kötelező',
+            'worker_id.unique' => 'Dolgozó már regisztrálva van osztályvezetőnek.',
+            'status_id.required' => 'Státusz megadása kötelező.',
+        ];
+    }
+
+    public function set_department_manager($department_manager_id) {
+        $department_manager = DepartmentManager::where('id', '=', $department_manager_id)->first();
+
+        $this->worker_id = $department_manager->worker_id;
+        $this->status_id = $department_manager->status_id;
+
+        $worker = Worker::where('id', '=', $this->worker_id)->first();
+
+        $this->worker_name = $worker->name;
+        $this->registration_number = $worker->registration_number;
+    }
+
+    public function create() {
+        $this->validate($this->rules(), $this->messages());
+
+        $department_manager = DepartmentManager::create([
+            'worker_id' => $this->worker_id,
+            'status_id' => $this->status_id
+        ]);
+
+        $department_manager->save();
+
+        $this->reset();
+    }
+
+    public function delete() {
+        $department_manager = DepartmentManager::where("worker_id", "=", $this->worker_id)->first();
+
+        $department_manager->delete();
+
+        $this->reset();
+    }
+
+    public function update() {
+        $this->validate($this->update_rules, $this->messages());
+
+        $department_manager = DepartmentManager::where("worker_id", "=", $this->worker_id)->first();
+
+        $departments_with_this_manager = Department::where("department_manager_id", "=", $department_manager->id)->where('status_id', '=', 1)->get();
+
+        if ($this->status_id == 2 && count($departments_with_this_manager) > 0)
+            throw ValidationException::withMessages([
+                'form.status_id' => ["Az osztályvezető 1 vagy több aktív osztályhoz van rendelve."]
+            ]);
+
+        $department_manager->status_id = $this->status_id;
+
+        $department_manager->save();
+    }
 }
