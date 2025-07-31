f28525e97c7790971029d55e52c46489b3d014c2
Dazannah <beattius@gmail.com>
2025-07-31 20:24:07 +0200

    trait WorkerLike use custom typecast

--- app/Traits/WorkerLike.php
+++ app/Traits/WorkerLike.php
@@ -2,9 +2,20 @@
 
 namespace App\Traits;
 
+use App\Casts\SanitizedBool;
+use App\Casts\SanitizedInt;
+use App\Casts\SanitizedString;
 use App\Models\Department;
 
 trait WorkerLike {
+  protected $commonWorkerCasts = [
+    'name' => SanitizedString::class,
+    'is_technical' => SanitizedBool::class,
+    'registration_number' => SanitizedString::class,
+    'post' => SanitizedString::class,
+    'department_id' => SanitizedInt::class
+  ];
+
   protected $commonWorkerAttributes = [
     'name',
     'is_technical',
@@ -15,6 +26,10 @@ trait WorkerLike {
 
   public function getWorkerLikeAttributes(): array {
     return $this->commonWorkerAttributes;
+  }
+
+  public function getWorkerLikeCasts(): array {
+    return $this->commonWorkerCasts;
   }
 
   public function department() {
