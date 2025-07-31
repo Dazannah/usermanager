4746c84b6e2bc21786ab32e322657ce0133b0ae1
Dazannah <beattius@gmail.com>
2025-07-31 20:13:10 +0200

    custom sanitization casts created

---
+++ app/Casts/SanitizedInt.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Casts;
+
+use InvalidArgumentException;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class SanitizedInt implements CastsAttributes {
+    /**
+     * Cast the given value.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function get(Model $model, string $key, mixed $value, array $attributes): mixed {
+        return $value;
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function set(Model $model, string $key, mixed $value, array $attributes): mixed {
+        if (is_numeric($value) && ctype_digit(strval($value)))
+            return intval($value);
+
+        throw new InvalidArgumentException("A(z) {$key} mező értéke érvénytelen egész szám: " . var_export($value, true));
+    }
+}
